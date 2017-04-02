2c0554f9166c2f0c9e4c3153fca40fe718321c67
Bobby <rhieger@nyc.rr.com>
2017-04-02 01:51:18 -0400

    Completed Example 2-2: Pages 41-42

---
+++ chapter-2/js/sketch1.js
@@ -0,0 +1,21 @@
+function setup()  {
+
+   createCanvas(550,120);
+
+}
+
+function draw()   {
+
+   if (mouseIsPressed) {
+
+      fill(0);
+
+   }  else {
+
+      fill(255);
+
+   }
+
+   ellipse(mouseX, mouseY, 80, 80);
+
+}
