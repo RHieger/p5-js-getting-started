9dc811adf0cb4a5fb002c872c8113a9391905da1
Bobby <rhieger@nyc.rr.com>
2017-04-02 01:13:14 -0400

    Completed Example 2-1: Pages 39-41

---
+++ chapter-2/js/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+
+   // Normally setup code for the canvas would go here.
+   // As this is the first rudimentary example, I will
+   // dispense with the setup and get right to the draw()
+   // function.
+
+}
+
+function draw() {
+
+   background(204);
+   ellipse(50,50,80,80);
+
+}
