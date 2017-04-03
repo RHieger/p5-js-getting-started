3e2695039ff837c7e37f4953f9febcafbd2b8772
Bobby <rhieger@nyc.rr.com>
2017-04-02 22:31:26 -0400

    Completed Example 3-2: Pages 53-54

---
+++ chapter-3/js/sketch2.js
@@ -0,0 +1,12 @@
+function setup()  {
+
+   createCanvas(480, 120);
+
+}  // end setup()
+
+function draw()   {
+
+   background(204);  // Set background color.
+   point(240, 60);   // Draw a point at X = 240, Y = 260.
+
+}  // end draw()
